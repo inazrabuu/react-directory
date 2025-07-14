d10919274374fbb4133651a2bd7f73b0f2aea116
inazrabuu <inazrabuu@gmail.com>
2025-07-14 15:13:35 +0700

    adding memo

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,12 @@
+import { useMemo } from "react"
+
 function ContactList({ contacts, search }) {
-  const filtered = contacts.filter(c => c.name.toLowerCase().includes(search.toLowerCase()))
+  // const filtered = contacts.filter(c => c.name.toLowerCase().includes(search.toLowerCase())) //without memoization
+
+  const filtered = useMemo(() => {
+    console.log('filtering contact ...')
+    return contacts.filter(c => c.name.toLowerCase().includes(search.toLowerCase()))
+  }, [contacts, search]) //with memmo
 
   return (
     <div>
